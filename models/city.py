cb49bd44a9e2b423131ac8532fa7487e896ea73e
wassimbel <wassim.belhedi1@gmail.com>
2020-08-13 17:12:11 +0000

    fix

--- models/city.py
+++ models/city.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
