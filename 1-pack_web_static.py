e9337496f7c84c3f862bf58f5120009ecbddf9bf
wassimbel <wassim.belhedi1@gmail.com>
2020-08-17 22:47:39 +0000

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,9 +9,9 @@ from datetime import datetime
 def do_pack():
     """ function """
     time = datetime.now().strftime('%Y%m%d%H%M%S')
-    local('mkdir versions')
-    f = 'versions/web_static_{}.tgz'.format(time)
-    local('tar czvf {} web_static'.format(f))
+    local("mkdir versions")
+    f = "versions/web_static_{}.tgz".format(time)
+    local("tar czvf {} web_static".format(f))
     if path.exists(f):
         return f
     else:
