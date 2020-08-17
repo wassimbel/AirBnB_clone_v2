64afac5246dbc5579ca5f05a03ecba4b3ce7bb27
wassimbel <wassim.belhedi1@gmail.com>
2020-08-17 22:55:11 +0000

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 def do_pack():
     """ function """
-    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir versions")
     f = "versions/web_static_{}.tgz".format(time)
     local("tar czvf {} web_static".format(f))
