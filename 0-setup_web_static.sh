ee0d8c05f9a834ed77a750add5b92e5cd9630156
wassimbel <wassim.belhedi1@gmail.com>
2020-08-18 03:52:05 +0000

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,6 +9,6 @@ sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '45i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
 sudo service nginx restart
