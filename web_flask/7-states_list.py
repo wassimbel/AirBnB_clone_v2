19bc5d81323fd4cffa18aecdcbfadcafa4a16c6b
wassimbel <wassim.belhedi1@gmail.com>
2020-09-02 14:30:16 +0000

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.teardown_appcontext
-def down():
+def down(self):
     """ After each request you must remove the current SQLAlchemy Session """
     storage.close()
 
@@ -15,7 +15,7 @@ def down():
 @app.route('/states_list', strict_slashes=False)
 def states():
     """display a HTML page: (inside the tag BODY) """
-    states = storage.all(State)
+    states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
