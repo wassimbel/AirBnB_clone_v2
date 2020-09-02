52ef0bd9ecb1c1aef3e1a82f0a05ad553bf0a893
wassimbel <wassim.belhedi1@gmail.com>
2020-09-02 14:14:20 +0000

    update

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" module - starts a Flask web application: """
+from models import storage
+from flask import FLASK, render_template
+
+
+app = FLASK(__name__)
+
+@app.teardown_appcontext
+    def down():
+        """ After each request you must remove the current SQLAlchemy Session """
+        storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+    def states():
+        """display a HTML page: (inside the tag BODY) """
+        states = storage.all(State)
+        return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__"
+    app.run(host="0.0.0.0", port="5000")
