dfb2d0ae577f5c9ea69a6647c8f85de5dbc5ea31
wassimbel <wassim.belhedi1@gmail.com>
2020-09-02 15:07:01 +0000

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 """ module - starts a Flask web application: """
 from models import storage
 from flask import Flask, render_template
-
+from models.state import State
 
 app = Flask(__name__)
 
@@ -16,7 +16,7 @@ def down(self):
 @app.route('/states_list', strict_slashes=False)
 def states():
     """display a HTML page: (inside the tag BODY) """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
