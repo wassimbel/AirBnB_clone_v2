3fb009e54729a377c33f354b3bcc542d250f7f69
wassimbel <wassim.belhedi1@gmail.com>
2020-09-02 13:03:05 +0000

    update

--- models/state.py
+++ models/state.py
@@ -6,7 +6,7 @@ from os import getenv
 import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
